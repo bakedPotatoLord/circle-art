a73c9f5b9756d78f142d9e6e3d66eb0d54915efa
BakedPotatoLord <918780@dpsk12.net>
2022-06-11 21:30:37 -0600

    made a basic version of the color-finding system

--- Circle.js
+++ Circle.js
@@ -7,7 +7,7 @@ export class Circle{
         this.y =Math.floor(Math.random()*cw)
         this.r = 0
 
-        if(ctx.getImageData(this.x,this.y,1,1).data[0] ){
+        if(ctx.getImageData(this.x,this.y,1,1).data[0] == 0 ){
             //if starts on black surface
             this.surfaceColor="black"
         }else{
@@ -23,17 +23,40 @@ export class Circle{
     }
 
     checkCollission(){
+        console.log("starting colision search")
 
 
         if(this.surfaceColor == "black"){
-            for(let x=0;x<cw;x++){
-                for(let y=0;y<ch;y++){
-                    if((x-this.x)^2+(y=this.y)){
-                        
+            //if starting on black
+            for(var x=0;x<cw;x++){
+                console.log(`${(x/cw)*100} percent`)
+                for(var y=0;y<ch;y++){
+                    if((x-this.x)^2+(y=this.y) > this.r && ctx.getImageData(x,y,1,1).data[0] == 255){
+                        //if it finds any white in the circle
+                        return true 
                     }
                 }
             }
+            //if it finds nothing white underneath the circle
+            return false
+
+
         }else{
+            //if starting on white
+            for(var x=0;x<cw;x++){
+                console.log(`${Math.floor((x/cw)*100)} percent`)
+                for(var y=0;y<ch;y++){
+                    
+
+                    if((x-this.x)^2+(y-this.y) > this.r && ctx.getImageData(x,y,1,1).data[0] == 0){
+                        //if it finds any black in the circle
+                        return true 
+
+                    }
+                }
+            }
+            //if it finds nothing white underneath the circle
+            return false
 
         }
 
